767adb4c05c90d9dc63df260c57bc24ecd56e82d
Andrey <andrey.salas@gmail.com>
2013-10-10 12:01:46 -0500

    Repositorio Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MotivateMe::Application.config.secret_key_base = '35a4ed55570ad4918e92c4e31cc755efb5c05898ab5bf1f19da296f5ae9c480f998b5d2147ab7741058195ccaeec41c2bda9420d82e8b0bd30d1632c712ddc78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MotivateMe::Application.config.session_store :cookie_store, key: '_motivate_me_session'
