e4f31f8fabecd30e463ca2e4ee5fee3faec969b2
Andrey <andrey.salas@gmail.com>
2013-10-21 12:32:08 -0500

    User model and security OK

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011003820) do
+ActiveRecord::Schema.define(version: 20131021171456) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
